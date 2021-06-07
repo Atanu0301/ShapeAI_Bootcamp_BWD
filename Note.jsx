5cc588d2085c39ae855f1db32218ce267b86e0cb
Atanu0301 <85478514+Atanu0301@users.noreply.github.com>
2021-06-07 12:27:02 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Performance based</h1>
+      <p>
+        POCO, formerly known as POCO by Xiaomi and Pocophone, is a Chinese
+        smartphone company. The Poco brand was first announced in August 2018 as
+        a mid-range smartphone line under Xiaomi. Poco India became an
+        independent company on 17 January 2020, followed by its global
+        counterpart in 24 November 2020.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
